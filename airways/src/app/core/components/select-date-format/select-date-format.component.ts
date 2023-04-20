61f6eb0f5ab56e2816804f84b753a93891aa9a84
Andrei Chabatul <andreichabatul@gmail.com>
2023-04-20 10:31:40 +0300

    feat: select date format

---
+++ airways/src/app/core/components/select-date-format/select-date-format.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-select-date-format',
+  templateUrl: './select-date-format.component.html',
+  styleUrls: ['./select-date-format.component.scss'],
+})
+export class SelectDateFormatComponent {
+ 
+  arrSelectFormatDate = ['MM/DD/YYYY', 'DD/MM/YYYY', 'YYYY/DD/MM', 'YYYY/MM/DD'];
+  selectFormatDate = this.arrSelectFormatDate[0];
+}
+
