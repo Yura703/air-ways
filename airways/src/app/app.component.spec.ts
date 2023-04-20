6b73e946cdb26e7e274e5480dc48b9b42ff96071
Andrei Chabatul <andreichabatul@gmail.com>
2023-04-20 10:36:24 +0300

    fix: feat app-component

--- airways/src/app/app.component.spec.ts
+++ airways/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'airways'`, () => {
+  it(`should have as title 'air-ways'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('airways');
+    expect(app.title).toEqual('air-ways');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('airways app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('air-ways app is running!');
   });
 });
--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -1,20 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SelectDateFormatComponent } from './core/components/select-date-format/select-date-format.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+import { MatSelectModule } from '@angular/material/select';
+import { SelectMoneyFormatComponent } from './core/components/select-money-format/select-money-format.component';
+import { LoginComponentComponent } from './auth/components/login-component/login-component.component';
+import { FormSearchFlightComponent } from './mainPage/components/form-search-flight/form-search-flight.component';
+import { MatRadioModule, MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MainPageComponent } from './mainPage/pages/main-page/main-page.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SelectDateFormatComponent,
+    SelectMoneyFormatComponent,
+    LoginComponentComponent,
+    FormSearchFlightComponent,
+    MainPageComponent,
+    FooterComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+    MatDatepickerModule,
+    [{
+      provide: MAT_RADIO_DEFAULT_OPTIONS,
+      useValue: { color: 'red' },
+    }]
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
