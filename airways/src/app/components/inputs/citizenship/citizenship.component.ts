703418a1378357469bd5f38809dc683a0f05a19c
guschins <guschins@gmail.com>
2023-04-23 13:38:49 +0200

    feat: add phone code citizenship component

---
+++ airways/src/app/components/inputs/citizenship/citizenship.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { PhoneCodeNameService } from '../../../services/phone-code-name.service';
+
+@Component({
+  selector: 'app-citizenship',
+  templateUrl: './citizenship.component.html',
+  styleUrls: ['./citizenship.component.scss'],
+})
+export class CitizenshipComponent implements OnInit {
+  @Output() citizenshipChange = new EventEmitter<string>();
+  optionsName: string[] = [];
+  selectedOption = '';
+
+  constructor(private phoneCodeNameService: PhoneCodeNameService) {}
+  ngOnInit() {
+    this.phoneCodeNameService.createArrayPhoneCodeName().subscribe((value) => {
+      value.forEach((el) => {
+        this.optionsName.push(el.name);
+      });
+    });
+  }
+}
