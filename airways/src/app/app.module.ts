efa6256e00abc6c3b24a326ada3641a7b8397153
guschins <guschins@gmail.com>
2023-04-23 14:33:34 +0200

    feat: add core module

--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
@@ -13,6 +13,7 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
