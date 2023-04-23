b49d7a580dd1817a933b314e4842b87eb798a050
guschins <guschins@gmail.com>
2023-04-23 13:26:01 +0200

    feat: add pop-up component

--- airways/src/app/app.component.ts
+++ airways/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { PopUpComponent } from './components/pop-up/pop-up/pop-up.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'airways';
+
+  private dialogRef!: MatDialogRef<PopUpComponent>;
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialogRef = this.dialog.open(PopUpComponent, {
+      width: '494px',
+    });
+  }
 }
--- airways/src/app/app.module.ts
+++ airways/src/app/app.module.ts
@@ -5,14 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { SignUpComponent } from './components/pop-up/sign-up/sign-up.component';
+import { FormsModule } from '@angular/forms';
+import { PhoneCodeCountryComponent } from './components/inputs/phone-code-country/phone-code-country.component';
+import { CitizenshipComponent } from './components/inputs/citizenship/citizenship.component';
+import { LogInComponent } from './components/pop-up/log-in/log-in.component';
+import { PopUpComponent } from './components/pop-up/pop-up/pop-up.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignUpComponent, PhoneCodeCountryComponent, CitizenshipComponent, LogInComponent, PopUpComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
