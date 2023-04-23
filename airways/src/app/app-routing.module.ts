0f0506696062be331d66ce06bc7dfe9e683ed102
guschins <guschins@gmail.com>
2023-04-23 14:34:20 +0200

    fix: change app routing module

--- airways/src/app/app-routing.module.ts
+++ airways/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './core/pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'flight-booking',
+    loadChildren: () =>
+      import('./flight-booking/flight-booking.module').then(
+        (m) => m.FlightBookingModule
+      ),
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then((m) => m.CartModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
