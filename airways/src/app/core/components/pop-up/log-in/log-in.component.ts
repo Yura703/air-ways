e4948e52cf8e0992cfef690b758255afb94da038
guschins <guschins@gmail.com>
2023-04-27 10:33:22 +0200

    fix: change validation for log-n component

--- airways/src/app/core/components/pop-up/log-in/log-in.component.ts
+++ airways/src/app/core/components/pop-up/log-in/log-in.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -20,11 +15,8 @@ export class LogInComponent implements OnInit {
 
   ngOnInit() {
     this.logInForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [
-        Validators.required,
-        this.strongPasswordValidator,
-      ]),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
     });
   }
 
@@ -33,37 +25,6 @@ export class LogInComponent implements OnInit {
   }
   get password() {
     return this.logInForm.get('password');
-  }
-
-  //проверяет ввод при изменении инпута
-  validateInput(key: any) {
-    key.markAsTouched();
-    key.updateValueAndValidity();
-  }
-
-  strongPasswordValidator(
-    control: AbstractControl
-  ): { [key: string]: boolean } | null {
-    const value = control.value;
-    const hasNumber = /[0-9]/.test(value);
-    const hasUppercase = /[A-Z]/.test(value);
-    const hasLowercase = /[a-z]/.test(value);
-    const hasSpecialCharacters = /[!@#?]/.test(value);
-
-    if (!value) {
-      return null;
-    }
-    if (
-      value &&
-      value.length >= 8 &&
-      hasNumber &&
-      hasUppercase &&
-      hasLowercase &&
-      hasSpecialCharacters
-    ) {
-      return null;
-    }
-    return { strongPassword: true };
   }
 
   onSubmit(form: FormGroup) {
