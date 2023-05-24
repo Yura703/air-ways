da031bde089c2aa4cdf4afa2b4910374f0aa7a88
guschins <guschins@gmail.com>
2023-05-24 13:26:34 +0200

    fix: change BASIC_URL_TRAVELPAYOUTS

--- airways/src/app/store/index.ts
+++ airways/src/app/store/index.ts
@@ -1,25 +1,24 @@
-import { ActionReducerMap } from "@ngrx/store";
-import { IOptionsSearch } from "./models/optionsSearch";
-import { IAppStore, IState } from "./models/stateModel";
-import { Reducers } from "./reducers/reducers";
+import { ActionReducerMap } from '@ngrx/store';
+import { IOptionsSearch } from './models/optionsSearch';
+import { IAppStore, IState } from './models/stateModel';
+import { Reducers } from './reducers/reducers';
 
 export const stateApp: IState = {
-    dateFlight: [],
-    searchMain: {} as IOptionsSearch,
-    errorLoading: false,
-    exchangeRate: 1,
-    moneyFormat: 'RUB',
+  dateFlight: [],
+  searchMain: {} as IOptionsSearch,
+  errorLoading: false,
+  exchangeRate: 1,
+  moneyFormat: 'RUB',
 };
 
 export const initalState: IAppStore = {
-    state: stateApp,
+  state: stateApp,
 };
-
+//у меня проверка пишет, что не использвется эта функция
 export function getInitalState(): IAppStore {
-    return initalState;
+  return initalState;
 }
 
 export const appReducers: ActionReducerMap<IAppStore, any> = {
-    state: Reducers,
+  state: Reducers,
 };
-
