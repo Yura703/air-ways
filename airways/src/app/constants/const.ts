89a5b534cd9e633defe7a59884a111defd12f0a6
guschins <guschins@gmail.com>
2023-05-28 10:25:13 +0200

    fix: change url api

--- airways/src/app/constants/const.ts
+++ airways/src/app/constants/const.ts
@@ -2,6 +2,6 @@ export const BASIC_URL_AUTOCOMPLETE =
   'https://autocomplete.travelpayouts.com/places2?locale=en&term='; // добавить locale в интерцептор пото
 export const BASIC_URL_TRAVELPAYOUTS =
   // 'http://localhost:4200/api/v2/prices/latest?limit=10&one_way=false&token=87ce13d373824fa9a73476fd2bcba858';
-  'http://api.travelpayouts.com/aviasales/v3/prices_for_dates?&unique=false&sorting=price&direct=false&cy=usd&limit=10&page=1&one_way=false&token=87ce13d373824fa9a73476fd2bcba858';
+  'https://api.travelpayouts.com/aviasales/v3/prices_for_dates?&unique=false&sorting=price&direct=false&cy=usd&limit=10&page=1&one_way=false&token=87ce13d373824fa9a73476fd2bcba858';
 export const BASIC_URL_CURRATE =
   'https://cors-anywhere.herokuapp.com/https://currate.ru/api/?get=rates&key=8df17c354fcb5dce55cbb3e7d1cc3728&pairs=RUB'; // добавить locale в интерцептор потом
