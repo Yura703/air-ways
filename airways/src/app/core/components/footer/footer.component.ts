1c26913380664bf9907624063fc2fe6ba418f047
guschins <guschins@gmail.com>
2023-05-28 08:16:04 +0200

    fix: change links to github

--- airways/src/app/core/components/footer/footer.component.ts
+++ airways/src/app/core/components/footer/footer.component.ts
@@ -2,53 +2,50 @@ import { Component } from '@angular/core';
 
 interface IfooterDataIco {
   img: string;
-  link: string
+  link: string;
 }
 
 interface IfooterPayIco {
   img: string;
 }
 
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
   footerDataIco: IfooterDataIco[] = [
     {
-      img: './../../../../assets/RSlogo.png',
+      img: '/assets/RSlogo.png',
       link: 'https://rs.school/',
     },
     {
-      img: './../../../../assets/oneGit.png',
-      link: 'https://github.com/IharAnt',
+      img: '/assets/oneGit.png',
+      link: 'https://github.com/yura703',
     },
     {
-      img: './../../../../assets/twoGit.png',
-      link: 'https://github.com/Stepan9092',
-    },
-    {
-      img: './../../../../assets/threeGit.png',
+      img: '/assets/twoGit.png',
       link: 'https://github.com/andreiChabatul',
-    }
-  ]
+    },
+    {
+      img: '/assets/threeGit.png',
+      link: 'https://github.com/guschins',
+    },
+  ];
 
   footerPayIco: IfooterPayIco[] = [
     {
-      img: './../../../../assets/iconVisa.svg'
+      img: '/assets/iconVisa.svg',
     },
     {
-      img: './../../../../assets/iconMastercard.svg'
+      img: '/assets/iconMastercard.svg',
     },
     {
-      img: './../../../../assets/iconAmasonPay.svg'
+      img: '/assets/iconAmasonPay.svg',
     },
     {
-      img: './../../../../assets/iconAmerican.svg'
-    }
-  ]
-
+      img: '/assets/iconAmerican.svg',
+    },
+  ];
 }
